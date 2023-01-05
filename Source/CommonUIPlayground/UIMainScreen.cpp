4f1f4e8c47d54240d086ab31a4d68bbf4448572e
Snorri Sturluson <snorri.sturluson@gmail.com>
2023-01-05 22:04:21 +0000

    Fix crash issue with native gameplay tag when not in editor

--- Source/CommonUIPlayground/UIMainScreen.cpp
+++ Source/CommonUIPlayground/UIMainScreen.cpp
@@ -6,13 +6,14 @@
 #include "UITag.h"
 #include "WindowManager.h"
 #include "Input/CommonUIInputTypes.h"
+#include "NativeGameplayTags.h"
 
-static const FUIActionTag EscapeActionTag = FUIActionTag::AddNativeTag(TEXT("Escape"));
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_UI_ACTION_ESCAPE, "UI.Action.Escape");
 
 void UUIMainScreen::NativeOnInitialized()
 {
 	Super::NativeOnInitialized();
-	RegisterUIActionBinding(FBindUIActionArgs(EscapeActionTag, false, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleEscapeAction)));
+	RegisterUIActionBinding(FBindUIActionArgs(FUIActionTag::ConvertChecked(TAG_UI_ACTION_ESCAPE), false, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleEscapeAction)));
 }
 
 void UUIMainScreen::HandleEscapeAction_Implementation()
